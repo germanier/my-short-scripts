070a3d8c2af52a3490263234dbb4cc68e1a41015
Marco Germanier <germanier.rex@gmail.com>
2021-10-28 21:55:08 +0100

    first cool commit to this -more to be added soon-

---
+++ install-script.sh
@@ -0,0 +1,38 @@
+# /bin/bash
+
+# force update system packages
+yay -Syyu
+
+# install some core basics
+yay -S neofetch htop git
+
+# install some text editors
+# dont hate once i understand vim ill use that
+yay -S micro vscodium-bin vscodium-bin-marketplace
+
+# install backup web browsers
+yay -S vivaldi brave-browser tor-browser
+
+# install document editors
+yay -S onlyoffice libreoffice-fresh
+
+# install microsoft fonts (update with w10/11 tbc)
+yay -S ttf-ms-fonts
+
+# install media stuff
+yay -S mpv yt-dlp
+
+# install recording stuff
+yay -S obs-studio
+
+# install proprietary junk
+yay -S teams-for-insiders
+
+# git stuff
+mkdir Documents/git
+cd Documents/git
+
+git clone 
+
+cd
+
