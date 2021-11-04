b0606b6cea65b8aab9668b8da3cc7196d768af9f
Marco Germanier <marco@mrgt.me>
2021-11-04 18:48:21 +0000

    added scripts from other computer

--- moodle-github-updater.sh
+++ moodle-github-updater.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-$FROM=/home/rxx/Downloads/Moodle
+$FROM=/home/rxx/Downloads/Moodle/
 $TO=/home/rxx/Documents/git/UoBuckingham-RepoDump/Term\ 9/
 
-rsync -av $FROM $TO
+cd $FROM
+rsync -av * $TO
 
 cd $TO
 cd ..
---
+++ teamsdestreamer.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+cd /home/rxx/Documents/git/destreamer/
+npm run build
+./destreamer.sh -f /home/rxx/Videos/Recordings/list.txt -u "1909602@buckingham.ac.uk" -k --skip -o /home/rxx/Videos/Recordings/
