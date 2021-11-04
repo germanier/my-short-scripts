4ef075d6b7bfe7d52cee6486fe2beadda70f4acf
Marco Germanier <marco@mrgt.me>
2021-11-04 22:59:48 +0000

    updated teamsdestreamer to new system

--- teamsdestreamer.sh
+++
@@ -1,4 +0,0 @@
-# /bin/sh
-cd /home/rxx/Documents/git/destreamer/
-npm run build
-./destreamer.sh -f /home/rxx/Videos/Recordings/list.txt -u "1909602@buckingham.ac.uk" -k --skip -o /home/rxx/Videos/Recordings/
---
+++ teamsdestreamer/teamsdestreamer.sh
@@ -0,0 +1,4 @@
+# /bin/sh
+cd /home/rxx/Documents/git/destreamer/
+npm run build
+./destreamer.sh -f /home/rxx/Documents/git/my-short-scripts/teamsdestreamer/list.txt -u "1909602@buckingham.ac.uk" -k --skip -o /home/rxx/Videos/Recordings/
