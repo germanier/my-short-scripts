763d68fd6da9d05443a83568eca3b11821499352
Marco Germanier <marco@mrgt.me>
2021-11-09 17:22:07 +0000

    changed moodle

--- moodle-github-updater.sh
+++ moodle-github-updater.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-$FROM=/home/rxx/Downloads/Moodle/
-$TO=/home/rxx/Documents/git/UoBuckingham-RepoDump/Term\ 9/
-
-cd $FROM
-rsync -av * $TO
-
-cd $TO
-cd ..
+cd /home/rxx/Downloads/Moodle/
+rsync -av * /home/rxx/Documents/git/UoBuckingham-RepoDump/Term\ 9/
+cd /home/rxx/Documents/git/UoBuckingham-RepoDump
 git add .
 git commit . -m "automatic moodle updater"
 git push
